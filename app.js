a1fd79f993d6b919c171fc86ed8232107147d660
Hernan Isaac Peñaranda Villarroel <isaacpv962@gmail.com>
2026-05-24 11:28:25 -0400

    feat: commit inicial del portafolio

---
+++ app.js
@@ -0,0 +1,3 @@
+document.getElementById('btn-interactivo').addEventListener('click', () => {
+  document.getElementById('mensaje-estado').textContent = '✅ CI/CD configurado correctamente. ¡El caché ha sido invalidado!';
+});
